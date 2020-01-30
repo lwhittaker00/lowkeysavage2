643d8af1f454e80871a713a3bea805db24e24405
Late'jah Whittaker <latejah.whittaker.938@my.csun.edu>
2020-01-29 20:23:24 -0800

    Initial Commit

---
+++ lowkeysavage2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lowkeysavage2
+//
+//  Created by User on 1/29/20.
+//  Copyright © 2020 User. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ lowkeysavage2Tests/lowkeysavage2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  lowkeysavage2Tests.swift
+//  lowkeysavage2Tests
+//
+//  Created by User on 1/29/20.
+//  Copyright © 2020 User. All rights reserved.
+//
+
+import XCTest
+@testable import lowkeysavage2
+
+class lowkeysavage2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
